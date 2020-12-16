38e7a8a91cc8c043c0050b9093de35ab7b86bfbc
Igbathscaller <Igbathscaller@gmail.com>
2020-12-16 13:07:41 -0500

    first test

--- Sorts.java
+++ Sorts.java
@@ -3,6 +3,23 @@ public class Sorts{
     *@postcondition The array will be modified such that the elements will be in increasing order.
     *@param data  the elements to be sorted.
     */
-    public static void bubbleSort(int[] data){  }
+    public static void bubbleSort(int[] data){
+        int stop = data.length-2;
+        int high=0;
+        int temp;
+        while(stop>0){
+            for(int j = 0; j<stop; j++){
+                if(data[j]>data[j+1]){
+                    temp=data[j];
+                    data[j]=data[j+1];
+                    data[j+1]=temp;
+                    high = j;
+                }
+            }
+            stop = high;
+        }
+
+    }
+
 }
   
